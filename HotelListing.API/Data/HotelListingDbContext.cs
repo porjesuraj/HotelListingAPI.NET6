640e4a775a61c83f28eaa84d7ee7a9452ba78d7a
porjesuraj <porjesuraj5@gmail.com>
2022-09-03 19:19:10 +0530

    Added Database context and initail data models

---
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext:DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+
+        public DbSet<Country> Countries { get; set; }
+    }
+}
