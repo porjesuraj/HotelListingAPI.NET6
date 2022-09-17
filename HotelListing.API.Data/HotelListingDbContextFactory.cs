e8cd37eac0b36904477d388bd1bcec7d288f505c
porjesuraj <porjesuraj5@gmail.com>
2022-09-17 22:30:23 +0530

    Fix as scaffolding was not working after dbcontext moved to different project

---
+++ HotelListing.API.Data/HotelListingDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContextFactory : IDesignTimeDbContextFactory<HotelListingDbContext>
+    {
+        public HotelListingDbContext CreateDbContext(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json",optional:false,reloadOnChange:true)
+                 .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<HotelListingDbContext>();
+            optionsBuilder.UseSqlServer(config.GetConnectionString("HotelListingDbConnectionString"));
+            return new HotelListingDbContext(optionsBuilder.Options);
+        }
+    }
+}
